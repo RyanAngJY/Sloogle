096ffa046f83bbf78db7cc8ecf02d8ef334b3375
Ryan <ryanang.jy@gmail.com>
2018-06-10 11:59:59 +0000

    Cleanup

--- public/js/controllers/appController.js
+++ public/js/controllers/appController.js
@@ -5,13 +5,11 @@ angular.module('sloogle').controller('AppCtrl', function ($scope, ImageService) 
 	// Initialisation
 	ImageService.getApiKey().then(function (response) {
 	    $scope.apiKey = response.data.unsplashApiKey;
-	    console.log(response.data.unsplashApiKey)
 	    $scope.searchForImages(1)
 	});
 
     $scope.searchForImages = function(number) {
     	var searchTerm = $scope.search;
-    	// 7bb02574f463dc1abb63fecf4941c51d5a2178af1cb85e6de38486c3d42d83a8
     	
     	$.ajax({
 	    	method: "GET",
